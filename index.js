07cf232fc00bae29808f69b270ff2c1ee5553c9a
sammarsaini <sammarsaini1009@gmail.com>
2024-09-16 19:54:11 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+}) 
+app.get('/login',(req,res)=>{
+    res.send('<h1>Welcome to the website.Please enter your login credentials')
+})
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${port}`)
+})
